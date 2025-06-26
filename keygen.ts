75fa0c1105702d33d2a5d8223690f60b959fdafe
Saksham Khare <sakshamkhare19@gmail.com>
2025-06-26 21:28:30 +0530

    Feat: keypair generate, conversion for encode and decode

---
+++ keygen.ts
@@ -0,0 +1,33 @@
+import { Keypair } from "@solana/web3.js";
+import bs58 from "bs58";
+import * as prompt from "prompt-sync";
+
+const ask = prompt();
+
+let kp = Keypair.generate();
+
+console.log(`You've generated a new Solana wallet: 
+${kp.publicKey.toBase58()} `);
+
+console.log(`[${kp.secretKey}]`);
+
+function walletToBase58(secretArray: number[]) {
+  const base58 = bs58.encode(Uint8Array.from(secretArray));
+  console.log("\n Base58 Private Key (for Phantom):");
+  console.log(base58);
+}
+
+function base58ToWallet() {
+  const base58Input = ask("\n🔁 Enter a base58 private key to decode: ");
+
+  try {
+    const decoded = bs58.decode(base58Input);
+    console.log("\n Decoded Wallet Byte Array:");
+    console.log(Array.from(decoded));
+  } catch (err) {
+    console.error("Invalid base58 input.");
+  }
+}
+
+walletToBase58(Array.from(kp.secretKey));
+base58ToWallet();
